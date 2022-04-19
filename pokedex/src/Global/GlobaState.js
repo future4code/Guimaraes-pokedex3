67bbcfac8c0946f16e968b48d500b791d648aa1e
CamilaCoradi <camila_coradi@hotmail.com>
2022-04-19 16:41:21 -0300

    implementação da página pokedex e detalhes

--- pokedex/src/Global/GlobaState.js
+++ pokedex/src/Global/GlobaState.js
@@ -1,82 +1,86 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {BASE_URL} from '../Constants/Url'
-import useRequestData from '../Hooks/UseRequestData'
-import GlobalStateContext from './GlobalStateContext'
+import { BASE_URL } from "../Constants/Url";
+import useRequestData from "../Hooks/UseRequestData";
+import GlobalStateContext from "./GlobalStateContext";
 
-const GlobalState =  (props) => {
-    const [pokemonsUrls, setPokemonsUrls] = useState([])
-    const [pokemonsDetails, setPokemonsDetails] = useState([])
-    const [pokedex, setPokedex] = useState([])
-    const [numberPage, setNumberPage] = useState(1);
-    const page = numberPage * 20;
-    const [pokemonData, setPokemonData] = useRequestData(`${BASE_URL}/pokemon?limit=20&offset=${page}`) 
+const GlobalState = (props) => {
+  const [pokemonsUrls, setPokemonsUrls] = useState([]);
+  const [pokemonsDetails, setPokemonsDetails] = useState([]);
+  const [pokedex, setPokedex] = useState([]);
+  const [numberPage, setNumberPage] = useState(1);
+  const page = numberPage * 20;
+  const [pokemonData, setPokemonData] = useRequestData(
+    `${BASE_URL}/pokemon?limit=20&offset=${page}`
+  );
 
-    const onChangePage = (e, value) => setNumberPage (value)
+  const onChangePage = (e, value) => setNumberPage(value);
 
-    useEffect(() => {
-        const list = []
-        pokemonData && pokemonData.results.map((poke) => {
-            list.push(poke.url)
-            setPokemonsUrls(list)
-        })
-    },[pokemonData])
-    
-    useEffect(() => {
-        const dataPokemon = []
-        pokemonsUrls && pokemonsUrls.map((poke) => {
-            axios
-            .get(poke)
-            .then((res) => {
-                dataPokemon.push(res.data)
-                if (dataPokemon.length === 20) {
-                    setPokemonsDetails(dataPokemon);
-                  }
-            })
-            .catch((err) => err)
-        },
-        [pokemonsUrls])
-    })
+  useEffect(() => {
+    const list = [];
+    pokemonData &&
+      pokemonData.results.map((poke) => {
+        list.push(poke.url);
+        setPokemonsUrls(list);
+      });
+  }, [pokemonData]);
 
-    const addToPokedex = (poke) => {
-        const pokeIndex = pokemonsDetails.findIndex((pokemon) => pokemon.name === poke.name)
-        const newPokemonList = [...pokemonsDetails]
-        newPokemonList.splice(pokeIndex, 1)
-        const newPokedexList = [...pokedex, pokemonsDetails[pokeIndex]]
-        setPokemonsDetails(newPokedexList)
-        setPokedex(newPokedexList)
+  useEffect(() => {
+    const dataPokemon = [];
+    pokemonsUrls &&
+      pokemonsUrls.map((poke) => {
+        axios
+          .get(poke)
+          .then((res) => {
+            dataPokemon.push(res.data);
+            if (dataPokemon.length === 20) {
+              setPokemonsDetails(dataPokemon);
+            }
+          })
+          .catch((err) => alert(err));
+      });
+  }, [pokemonsUrls])
 
-        alert('Você adicionou um pokemon!');
-    }
+  const addToPokedex = (poke) => {
+    const pokeIndex = pokemonsDetails.findIndex(
+      (pokemon) => pokemon.name === poke.name
+    );
+    const newPokemonList = [...pokemonsDetails];
+    newPokemonList.splice(pokeIndex, 1);
+    const newPokedexList = [...pokedex, pokemonsDetails[pokeIndex]];
+    setPokemonsDetails(newPokedexList);
+    setPokedex(newPokedexList);
 
-    const rmvPokemonCart = (poke) => {
-        const pokeIndex = pokedex.findIndex((pokemon) => pokemon.name === poke.name)
-        const newPokedexList = [...pokedex]
-        newPokedexList.splice(pokeIndex, 1)
-        const newPokemonList = [...pokemonsDetails, pokedex[pokeIndex]]
+    alert("Você adicionou um pokemon!");
+  };
 
-        setPokemonsDetails(newPokemonList)
-        setPokedex(newPokedexList)
+  const rmvPokemonCart = (poke) => {
+    const pokeIndex = pokedex.findIndex(
+      (pokemon) => pokemon.name === poke.name
+    );
+    const newPokedexList = [...pokedex];
+    newPokedexList.splice(pokeIndex, 1);
+    const newPokemonList = [...pokemonsDetails, pokedex[pokeIndex]];
 
-        alert('Você removeu um pokemon!');
+    setPokemonsDetails(newPokemonList);
+    setPokedex(newPokedexList);
 
-    }
+    alert("Você removeu um pokemon!");
+  };
 
-    const pokemon =  {
-        pokemonsDetails,
-        onChangePage,
-        pokedex,
-        addToPokedex,
-        rmvPokemonCart,
-      };
+  const pokemon = {
+    pokemonsDetails,
+    onChangePage,
+    pokedex,
+    addToPokedex,
+    rmvPokemonCart,
+  };
 
+  return (
+    <GlobalStateContext.Provider value={pokemon}>
+      {props.children}
+    </GlobalStateContext.Provider>
+  );
+};
 
-      return (
-        <GlobalStateContext.Provider value={pokemon}>
-             {props.children}
-        </GlobalStateContext.Provider>
-       
-      )
-}
-
-export default GlobalState
+export default GlobalState;
--- pokedex/src/pages/Details/Details.js
+++ pokedex/src/pages/Details/Details.js
@@ -1,14 +1,50 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { goBackHome, goToBack } from "../../Routes/coordinator/coordinator";
+import { useNavigate, useParams } from "react-router-dom";
+import { BASE_URL } from "../../Constants/Url";
+import useRequestData from "../../Hooks/UseRequestData";
+import { goToBack } from "../../Routes/coordinator/coordinator";
 
 const Details = () => {
   const navigate = useNavigate();
 
+  const params = useParams();
+  const [pokemon] = useRequestData(`${BASE_URL}/pokemon/${params.name}`);
+
+  const pokeType =
+    pokemon &&
+    pokemon.types.map((type) => {
+      return <p key={pokemon.id}>{type.type.name.toUpperCase()}</p>;
+    });
+
+  const pokeStats =
+    pokemon &&
+    pokemon.stats.map((stat) => {
+      return <p key={pokemon.id}>{stat.base_stat}</p>;
+    });
+
+  const pokeMoves =
+    pokemon &&
+    pokemon.moves.slice(0, 3).map((move) => {
+      return <p key={pokemon.id}>{move.move.name}</p>;
+    });
+
   return (
     <div>
-      <h1>Detalhes</h1>  
-      <button onClick={() => goBackHome(navigate)}>Página Inicial</button>
+      <h1>Detalhes</h1>
+
+      {pokemon && (
+        <div>
+         
+          <p>Nome: {pokemon.name}</p>
+          <p>Tipo: {pokeType}</p>
+          <p>Altura: {pokemon.height}</p>
+          <p>Peso: {pokemon.weight}</p>
+          <h4>S T A T S</h4>
+          {pokeStats}
+          <h4>Ataques</h4>
+          {pokeMoves}
+        </div>
+      )}
       <button onClick={() => goToBack(navigate)}>Voltar para Pokedex</button>
     </div>
   );
