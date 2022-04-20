ada06751485604052cb2cad7a6418992923185b9
Caio Rigotto <caiorigotto@gmail.com>
2022-04-19 21:35:13 -0300

    Mudada a cor do card de acordo com o tipo do pkm

--- pokedex/src/pages/Home/Home.js
+++ pokedex/src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import { goToPokedex } from "../../Routes/coordinator/coordinator";
 import GlobalStateContext from "../../Global/GlobalStateContext";
 import { goToDetails } from "../../Routes/coordinator/coordinator";
 import { Pagination } from "@mui/material";
-import { PkmCard, PkmContainer, PkmText } from "./StyledHome";
+import { PkmButtons, PkmButtonsContainer, PkmCard, PkmContainer, PkmImg, PkmText } from "./StyledHome";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -18,19 +18,26 @@ const Home = () => {
       {pokemonsDetails?.map((poke) => {
         console.log(poke)
         return (
-          <PkmCard url={poke.sprites.other.home.front_default}>
+          <PkmCard type={poke.types[0].type.name}>
+            <PkmImg
+              width={"150px"}
+              src={poke.sprites.other.home.front_default}
+              alt={pokemonsDetails.name}
+            />
             <PkmText key={poke.id}>{poke.name}</PkmText>
             <PkmText>{poke.types[0].type.name}</PkmText>
-            <button onClick={() => addToPokedex(poke)}>
+            <PkmButtonsContainer>
+            <PkmButtons onClick={() => addToPokedex(poke)}>
               {" "}
               Adicionar à Pokedex{" "}
-            </button>
-            <button
+            </PkmButtons>
+            <PkmButtons
               onClick={() => goToDetails(navigate, poke.name)}
               key={poke.name}
             >
-              Ver Detalhes
-            </button>
+              Detalhes
+            </PkmButtons>
+            </PkmButtonsContainer>
           </PkmCard>
         );
       })}
--- pokedex/src/pages/Home/StyledHome.js
+++ pokedex/src/pages/Home/StyledHome.js
@@ -5,16 +5,44 @@ export const PkmContainer = styled.div`
     flex-wrap: wrap;
     width: 100%;
 `
-
+const handleType = type =>{
+    switch(type){
+        case 'normal':
+        case 'ghost':
+            return "#EFEFF2";
+        case 'fire':
+        case 'fighting':
+        case 'dragon':
+            return "#F76545";
+        case 'electric':
+            return "#F7C545";
+        case 'ground':
+            return "#9B897B";
+        case 'ice':
+        case 'water':
+            return "#76AADB";
+        case 'grass':
+        case 'bug':
+            return "#70A83B";
+        case 'stile':
+        case 'dark':
+        case 'rock':
+            return "#A2A2A2"
+        case 'poison':
+        case 'psychic':
+        case 'fairy':
+        case 'ghost':
+            return "#A974BC";
+        default:
+            return "#EFEFF2";
+    }
+}
 export const PkmCard = styled.div`
-    background-image: url(${props => props.url});
-    background-repeat: no-repeat;
-    background-size: 80% 80%;
-    background-position: center;
-    border: 1px black solid;
-    border-radius: 5px;
+
+    background: ${props => handleType(props.type)};
+    border-radius: 40px;
     max-height: 30%;
-    height: 40vh;
+    height: fit-content;
     width: 15%;
     margin-left: auto;
     margin-right: auto;
@@ -22,8 +50,27 @@ export const PkmCard = styled.div`
     display: flex;
     flex-direction: column;
 `
-
+export const PkmImg = styled.img`
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10px;
+    background: #c7c7d1;
+    box-shadow: inset 6px 5px 7px rgba(0, 0, 0, 0.29);
+    border-radius: 70px;
+`
 export const PkmText = styled.p`
     text-transform: capitalize;
     text-align: center;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+`
+export const PkmButtonsContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+export const PkmButtons = styled.button`
+    border: none;
+    background-color: #c7c7d1;
 `
